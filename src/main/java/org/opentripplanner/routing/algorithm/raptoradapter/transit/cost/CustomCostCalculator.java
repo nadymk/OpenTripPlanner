22b93a83a1631d21fffaef6d37d361d2670a3aca
jon <jon-joshua@live.com>
2024-08-26 21:29:57 +0100

    log more data

--- src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/cost/CustomCostCalculator.java
+++ src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/cost/CustomCostCalculator.java
@@ -32,10 +32,13 @@ public class CustomCostCalculator<T extends DefaultTripSchedule>
   ) {
     trip.pattern().stopIndex(boardStop);
 
-    System.out.println(((TripScheduleWithOffset)trip).getOriginalTripPattern().getName());
-    System.out.println(((TripScheduleWithOffset)trip).getOriginalTripPattern().getStop(boardStop));
-    System.out.println(((TripScheduleWithOffset)trip).getOriginalTripPattern().getStop(boardStop).getId());
-    System.out.println(((TripScheduleWithOffset)trip).getOriginalTripPattern().getStops());
+    if (trip instanceof TripScheduleWithOffset) {
+      System.out.println(((TripScheduleWithOffset) trip).getOriginalTripPattern().getName());
+      System.out.println(((TripScheduleWithOffset) trip).getOriginalTripPattern().getStop(boardStop));
+      System.out.println(((TripScheduleWithOffset) trip).getOriginalTripPattern().getStop(boardStop));
+      System.out.println(((TripScheduleWithOffset) trip).getOriginalTripPattern().getStop(boardStop).getId());
+      System.out.println(((TripScheduleWithOffset) trip).getOriginalTripPattern().getStops());
+    }
 
     int defaultCost = delegate.boardingCost(
       firstBoarding,
